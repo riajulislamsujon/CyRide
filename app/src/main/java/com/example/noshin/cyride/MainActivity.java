4666b441702bcfed8efeac06e2d873d507b25195
NoshinTasnim <noshintasnimgrtr5@gmail.com>
2018-09-01 09:41:10 -0700

    Initial commit

---
+++ app/src/main/java/com/example/noshin/cyride/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.noshin.cyride;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signup = (TextView) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), signup.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
