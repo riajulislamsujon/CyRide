4203b862757a1c5f91af9302448df10f6eccc432
NoshinTasnim <noshintasnimgrtr5@gmail.com>
2018-09-03 20:30:58 -0700

    Added barcode layout

---
+++ app/src/main/java/com/example/noshin/cyride/barcodeactivity.java
@@ -0,0 +1,14 @@
+package com.example.noshin.cyride;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class barcodeactivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcodeactivity);
+    }
+
+}
