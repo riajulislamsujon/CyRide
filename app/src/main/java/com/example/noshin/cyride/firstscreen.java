77895ab99a59edf043751db7ca0ec1b353c03340
NoshinTasnim <noshintasnimgrtr5@gmail.com>
2018-09-05 00:51:14 -0700

    start screen firebase

--- app/src/main/java/com/example/noshin/cyride/firstscreen.java
+++ app/src/main/java/com/example/noshin/cyride/firstscreen.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.SystemClock;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class firstscreen extends Activity {
 
@@ -11,10 +14,16 @@ public class firstscreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firstscreen);
-        SystemClock.sleep(3000);
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+        img = findViewById(R.id.cycle);
+        img.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private ImageView img;
 
 }
