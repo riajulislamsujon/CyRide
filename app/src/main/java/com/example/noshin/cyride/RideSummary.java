5533ce06096e790331270ea55d7a48d8320403c0
shafin034 <shafinmist@gmail.com>
2018-09-06 04:48:40 +0600

    stopwatch 1

---
+++ app/src/main/java/com/example/noshin/cyride/RideSummary.java
@@ -0,0 +1,13 @@
+package com.example.noshin.cyride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RideSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_summary);
+    }
+}
--- app/src/main/java/com/example/noshin/cyride/StopwatchActivity.java
+++ app/src/main/java/com/example/noshin/cyride/StopwatchActivity.java
@@ -4,13 +4,17 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
 
     private Chronometer chronometer;
     private long pauseoffset;
     private boolean running;
+    TextView cycleId;
+    Button stopBtn;
 
 
     @Override
@@ -19,9 +23,20 @@ public class StopwatchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stopwatch);
 
         chronometer = findViewById(R.id.chronometer);
+        cycleId = findViewById(R.id.cycleId);
+        cycleId.setText("Cycle ID : "+getIntent().getStringExtra("cycleId"));
+        startChronometer();
+        stopBtn = findViewById(R.id.stopBtn);
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               stopChronometer();
+            }
+        });
     }
 
-    public void startChronometer(View v){
+    public void startChronometer(){
         if(!running){
             chronometer.setBase(SystemClock.elapsedRealtime() - pauseoffset);
             chronometer.start();
@@ -30,7 +45,7 @@ public class StopwatchActivity extends AppCompatActivity {
 
     }
 
-    public void stopChronometer(View v){
+    public void stopChronometer(){
         if(running){
             chronometer.stop();
             pauseoffset=SystemClock.elapsedRealtime() - chronometer.getBase();
